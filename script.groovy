499829cff9b90761fd67e43afbd1d7640f16e562
mostafa12345 <129852848+mostafa12345@users.noreply.github.com>
2024-08-14 18:33:02 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t USER_NAME/demo-app:jma-2.0 .'
+        sh 'docker build -t mostafataha12/app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mostafataha12/app:jma-2.0'
     }
